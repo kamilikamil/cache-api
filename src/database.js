c3f042e3215448ae2184d2446569e00e28acdffc
Kamil Kamili <kamili.kamil@outlook.com>
2022-03-18 22:02:26 +0400

    added insert item to db

--- src/database.js
+++ src/database.js
@@ -14,6 +14,82 @@ function getNewDate() {
 
   return new Date(date);
 }
+//inserts data for a given key
+function insertData(key, value) {
+  return new Promise((resolve, reject) => {
+    getAllItems(async function (error, result) {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      //check if cache is full
+      if (result.length > (process.env.MAX_ENTRIES || 10)) {
+        console.log("Cache is full. Will overwrite the key with oldest ttl");
+
+        return dbConnection
+          .collection("cache")
+          .find({})
+          .sort({ ttl: -1 })
+          .limit(1)
+          .toArray(async function (error, oldCache) {
+            if (error) {
+              reject(error);
+              return;
+            }
+            const cache = oldCache[0];
+
+            const date = getNewDate();
+
+            try {
+              await updateData(cache.key, {
+                value,
+                ttl: date,
+              });
+
+              resolve({ key: cache.key, value, state: "updated" });
+              return;
+            } catch (error) {
+              reject(error);
+              return;
+            }
+          });
+      }
+
+      const date = getNewDate();
+
+      const data = {
+        key,
+        value,
+        ttl: date,
+      };
+
+      try {
+        await dbConnection.collection("cache").insertOne(data);
+      } catch (error) {
+        if (error.code && error.code === 11000) {
+          console.error(`Key: ${key} already exists. Overwriting data`);
+          try {
+            await updateData(key, { value });
+
+            resolve({ key, value, state: "updated" });
+          } catch (error) {
+            console.dir(error);
+            reject(error);
+          }
+        } else {
+          console.error("Error writing data");
+          reject(error);
+        }
+
+        return;
+      }
+
+      return resolve({ key, value, state: "inserted" });
+    });
+  });
+}
+
 //updates the data for a given key
 async function updateData(key, value) {
   try {
