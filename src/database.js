c94d9148ace1107724fe60ec58a21811bfa6057c
Kamil Kamili <kamili.kamil@outlook.com>
2022-03-18 22:01:13 +0400

    added get items from db

--- src/database.js
+++ src/database.js
@@ -9,6 +9,20 @@ const client = new MongoClient(connectionString, {
 
 let dbConnection;
 
+function getAllItems(callback) {
+  dbConnection
+    .collection("cache")
+    .find({})
+    .toArray(function (error, result) {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      callback(null, result);
+      return;
+    });
+}
+
 module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
